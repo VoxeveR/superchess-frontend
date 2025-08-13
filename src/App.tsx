2f7b924ddeca72ac24c0056790b42e4274c53a04
VoxeveR <piotrzietek01@gmail.com>
2025-08-13 13:28:43 +0200

    initial login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ChessGamePage from "./ChessGamePage.tsx";
 import StarterPage from "./StarterPage.tsx";
+import LoginPage from "./LoginPage.tsx";
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<StarterPage />}></Route>
+                <Route path="/login" element={<LoginPage />}></Route>
                 <Route path='game/:gameCode' element={<ChessGamePage />} />
             </Routes>
         </BrowserRouter>
